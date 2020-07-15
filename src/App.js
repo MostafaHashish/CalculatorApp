af73ad7c9146d50da6c60e640c5d5b6c1ec1d24c
MostafaHashish <33876938+MostafaHashish@users.noreply.github.com>
2020-07-16 02:27:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './App.css';
+import Result from './Calculator/Result';
+import Keypad from './Calculator/Keypad';
+
+class App extends Component {
+ 
+       state = {
+            result: ""
+        }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "ERROR! Press C."
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <title>Calculator</title>
+                    <h1>Calculator App</h1>
+                    <Result result={this.state.result}/>
+                    <Keypad onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
